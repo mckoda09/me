c8af1173f6b41b0dbea1f6665164bb3dc8a72c00
Aleksii Nikitin <mckoda09@gmail.com>
2025-03-22 20:15:29 +0500

    nice

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,17 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
-import tailwind from '@astrojs/tailwind';
+import tailwindcss from '@tailwindcss/vite';
+
+import alpinejs from '@astrojs/alpinejs';
 
 import icon from 'astro-icon';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), icon()]
+  vite: {
+    plugins: [tailwindcss()]
+  },
+
+  integrations: [alpinejs(), icon()]
 });
